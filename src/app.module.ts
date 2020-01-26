5af5ece5ccb460326ebc7e1ee7e16c5ae770dee1
NTakuya <nagahama.takuya@outlook.jp>
2020-01-27 01:51:16 +0900

    feat(dockernise) add typeorm module and basic setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db-server',
+      port: 3306,
+      username: 'develop',
+      password: 'password',
+      database: 'develop',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
